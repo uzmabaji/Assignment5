dfd6221d0f1eee8af099f227f5561f3d84833a2c
uzmabaji <bhootbhai88@gmail.com>
2026-02-02 19:30:09 +0500

    Refactor app.js to use AngularJS controllers and services

--- app.js
+++ app.js
@@ -1,67 +1,57 @@
-(function (global) {
+(function () {
+'use strict';
 
-// Set up a namespace for our utility
-var ajaxUtils = {};
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
+// TO BUY CONTROLLER
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var buy = this;
 
-// Returns an HTTP request object
-function getRequestObject() {
-  if (window.XMLHttpRequest) {
-    return (new XMLHttpRequest());
-  } 
-  else if (window.ActiveXObject) {
-    // For very old IE browsers (optional)
-    return (new ActiveXObject("Microsoft.XMLHTTP"));
-  } 
-  else {
-    global.alert("Ajax is not supported!");
-    return(null); 
-  }
+  buy.items = ShoppingListCheckOffService.getToBuyItems();
+
+  buy.buyItem = function (itemIndex) {
+    ShoppingListCheckOffService.buyItem(itemIndex);
+  };
 }
 
+// ALREADY BOUGHT CONTROLLER
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
 
-// Makes an Ajax GET request to 'requestUrl'
-ajaxUtils.sendGetRequest = 
-  function(requestUrl, responseHandler, isJsonResponse) {
-    var request = getRequestObject();
-    request.onreadystatechange = 
-      function() { 
-        handleResponse(request, 
-                       responseHandler,
-                       isJsonResponse); 
-      };
-    request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+// SERVICE
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  var toBuyItems = [
+    { name: "cookies", quantity: 10 },
+    { name: "apples", quantity: 5 },
+    { name: "oranges", quantity: 5 },
+    { name: "bananas", quantity: 10 },
+    { name: "milk", quantity: 1 }
+  ];
+
+  var boughtItems = [];
+
+  service.buyItem = function (itemIndex) {
+    var item = toBuyItems.splice(itemIndex, 1)[0];
+    boughtItems.push(item);
   };
 
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
 
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
-function handleResponse(request,
-                        responseHandler,
-                        isJsonResponse) {
-  if ((request.readyState == 4) &&
-     (request.status == 200)) {
-
-    // Default to isJsonResponse = true
-    if (isJsonResponse == undefined) {
-      isJsonResponse = true;
-    }
-
-    if (isJsonResponse) {
-      responseHandler(JSON.parse(request.responseText));
-    }
-    else {
-      responseHandler(request.responseText);
-    }
-  }
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
 }
 
-
-// Expose utility to the global object
-global.$ajaxUtils = ajaxUtils;
-
-
-})(window);
-
+})();
